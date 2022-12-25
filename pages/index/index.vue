fdd203e8eede23adb18298caa071b471ef6e022e
lcruan <1416802331@qq.com>
2022-12-25 18:25:53 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+    hello uni-app
+    <text class="iconfont iconlike"></text>
+	</view>
+</template>
+
+<script>
+  import '@/common/iconfont.css';
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
