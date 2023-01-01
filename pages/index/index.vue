cc662b4da98027be33f9f9a84179c6953f1a7628
lcruan <1416802331@qq.com>
2023-01-01 18:47:40 +0800

    首页音乐分类布局及样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,6 +7,59 @@
           <text class="iconfont iconsearch"></text>
           <input type="text" placeholder="搜索歌曲" />
         </view>
+        <view class="index-list">
+          <view class="index-list-item">
+            <view class="index-list-img">
+              <image src="../../static/wangyiyunyinyue.png"></image>
+              <text>每天更新</text>
+            </view>
+            <view class="index-list-text">
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+            </view>
+          </view>
+          <view class="index-list-item">
+            <view class="index-list-img">
+              <image src="../../static/wangyiyunyinyue.png"></image>
+              <text>每天更新</text>
+            </view>
+            <view class="index-list-text">
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+            </view>
+          </view>
+          <view class="index-list-item">
+            <view class="index-list-img">
+              <image src="../../static/wangyiyunyinyue.png"></image>
+              <text>每天更新</text>
+            </view>
+            <view class="index-list-text">
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+              <view class="">
+                1.与我无关 - 阿如
+              </view>
+            </view>
+          </view>
+        </view>
       </scroll-view>
     </view>
 	</view>
@@ -52,5 +105,35 @@
 .index-search input {
   font-size: 28rpx;
   flex: 1;
+}
+.index-list {
+  margin: 0 30rpx;
+}
+.index-list-item {
+  display: flex;
+  margin-bottom: 34rpx;
+}
+.index-list-img {
+  width: 212rpx;
+  height: 212rpx;
+  position: relative;
+  border-radius: 30rpx;
+  overflow: hidden;
+  margin-right: 22rpx;
+}
+.index-list-img image {
+  width: 100%;
+  height: 100%;
+}
+.index-list-img text {
+  position: absolute;
+  left: 12rpx;
+  bottom: 16rpx;
+  color: #fff;
+  font-size: 20rpx;
+}
+.index-list-text {
+  font-size: 24rpx;
+  line-height: 66rpx;
 }
 </style>
